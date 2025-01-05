906fd65ad5c18300765624ec265cdf325500ea39
MaxMad446 <puper-1981@mail.ru>
2025-01-05 12:51:49 +0300

    Add documentation

--- src/Generated20260511.cpp
+++ src/Generated20260511.cpp
@@ -4,14 +4,14 @@ using namespace std;
 
 class Generated20260511 {
 public:
-    // Generated class 20260511_172836_657975
-    int value = 437;
-    string created = "20260511_172836_657975";
+    // Generated class 20260511_172844_590889
+    int value = 932;
+    string created = "20260511_172844_590889";
     
     int process() {
         int result = value;
-        for (int i = 0; i < 17; i++) {
-            result += i * 7;
+        for (int i = 0; i < 18; i++) {
+            result += i * 5;
         }
         return result;
     }
