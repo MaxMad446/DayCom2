bab7ba436241f9064f0151ad22640f83f01d815a
MaxMad446 <puper-1981@mail.ru>
2025-01-28 15:59:35 +0300

    Optimize loops

--- src/Generated20260511.cpp
+++ src/Generated20260511.cpp
@@ -4,14 +4,14 @@ using namespace std;
 
 class Generated20260511 {
 public:
-    // Generated class 20260511_173020_994297
-    int value = 405;
-    string created = "20260511_173020_994297";
+    // Generated class 20260511_173028_170652
+    int value = 744;
+    string created = "20260511_173028_170652";
     
     int process() {
         int result = value;
-        for (int i = 0; i < 8; i++) {
-            result += i * 8;
+        for (int i = 0; i < 23; i++) {
+            result += i * 3;
         }
         return result;
     }
