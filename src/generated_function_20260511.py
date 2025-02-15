f9c4d751a58f3943ae68ad1af0dec50ed3ced25f
MaxMad446 <puper-1981@mail.ru>
2025-02-15 17:11:53 +0300

    Refactor utility functions

--- src/generated_function_20260511.py
+++ src/generated_function_20260511.py
@@ -1,6 +1,6 @@
 def generated_function_20260511():
-    # Generated function 20260511_173150_921132
-    result = 149
-    for i in range(20):
-        result += i * 10
+    # Generated function 20260511_173158_127245
+    result = 180
+    for i in range(36):
+        result += i * 7
     return result
