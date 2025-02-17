ca75de8d271199e72b0621013744db9ea8774570
MaxMad446 <puper-1981@mail.ru>
2025-02-17 11:33:21 +0300

    Optimize loops

--- src/generated_function_20260511.py
+++ src/generated_function_20260511.py
@@ -1,9 +1,6 @@
-class Generated20260511:
-    # Generated class 20260511_173204_639594
-    
-    def __init__(self):
-        self.value = 419
-        self.created = "20260511_173204_639594"
-    
-    def process(self):
-        return self.value * 2
+def generated_function_20260511():
+    # Generated function 20260511_173205_664506
+    result = 525
+    for i in range(50):
+        result += i * 3
+    return result
