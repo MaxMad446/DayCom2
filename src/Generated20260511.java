c828f56dca393bb950f21b9d221c177d4f3a299e
MaxMad446 <puper-1981@mail.ru>
2025-02-23 11:11:02 +0300

    Optimize loops

--- src/Generated20260511.java
+++ src/Generated20260511.java
@@ -1,13 +1,13 @@
 public class Generated20260511 {
-    // Generated class 20260511_173234_904399
+    // Generated class 20260511_173236_716643
     
-    private int value = 638;
-    private String created = "20260511_173234_904399";
+    private int value = 101;
+    private String created = "20260511_173236_716643";
     
     public int process() {
         int result = value;
-        for (int i = 0; i < 23; i++) {
-            result += i * 7;
+        for (int i = 0; i < 35; i++) {
+            result += i * 3;
         }
         return result;
     }
