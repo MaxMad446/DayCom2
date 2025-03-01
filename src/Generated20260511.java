18ea1266b6694d0ad81c9c573045822cdd3d940a
MaxMad446 <puper-1981@mail.ru>
2025-03-01 12:42:58 +0300

    Fix edge case logic

--- src/Generated20260511.java
+++ src/Generated20260511.java
@@ -1,13 +1,13 @@
 public class Generated20260511 {
-    // Generated class 20260511_173300_306443
+    // Generated class 20260511_173308_633810
     
-    private int value = 301;
-    private String created = "20260511_173300_306443";
+    private int value = 406;
+    private String created = "20260511_173308_633810";
     
     public int process() {
         int result = value;
-        for (int i = 0; i < 44; i++) {
-            result += i * 8;
+        for (int i = 0; i < 47; i++) {
+            result += i * 2;
         }
         return result;
     }
