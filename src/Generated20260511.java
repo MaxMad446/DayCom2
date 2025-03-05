bf0dff1e0989917e489ef5a327f323c101a27660
MaxMad446 <puper-1981@mail.ru>
2025-03-05 16:15:21 +0300

    Refactor utility functions

--- src/Generated20260511.java
+++ src/Generated20260511.java
@@ -1,12 +1,12 @@
 public class Generated20260511 {
-    // Generated class 20260511_173324_401053
+    // Generated class 20260511_173325_117710
     
-    private int value = 199;
-    private String created = "20260511_173324_401053";
+    private int value = 182;
+    private String created = "20260511_173325_117710";
     
     public int process() {
         int result = value;
-        for (int i = 0; i < 49; i++) {
+        for (int i = 0; i < 44; i++) {
             result += i * 2;
         }
         return result;
