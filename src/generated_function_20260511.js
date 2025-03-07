78aae27796fb14239a433f71d7ade15202517c70
MaxMad446 <puper-1981@mail.ru>
2025-03-07 15:23:27 +0300

    Refactor utility functions

--- src/generated_function_20260511.js
+++ src/generated_function_20260511.js
@@ -1,8 +1,8 @@
 function generated_function_20260511() {
-    // Generated function 20260511_173334_242225
-    let result = 26;
-    for (let i = 0; i < 23; i++) {
-        result += i * 10;
+    // Generated function 20260511_173335_694218
+    let result = 224;
+    for (let i = 0; i < 31; i++) {
+        result += i * 3;
     }
     return result;
 }
