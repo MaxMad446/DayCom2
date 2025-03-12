6b443da9e84df9bdab7b2a4723bb1efef0a96074
MaxMad446 <puper-1981@mail.ru>
2025-03-12 14:21:42 +0300

    Add documentation

--- src/Generated20260511.java
+++ src/Generated20260511.java
@@ -1,13 +1,13 @@
 public class Generated20260511 {
-    // Generated class 20260511_173357_060197
+    // Generated class 20260511_173358_544232
     
-    private int value = 584;
-    private String created = "20260511_173357_060197";
+    private int value = 973;
+    private String created = "20260511_173358_544232";
     
     public int process() {
         int result = value;
-        for (int i = 0; i < 28; i++) {
-            result += i * 6;
+        for (int i = 0; i < 21; i++) {
+            result += i * 3;
         }
         return result;
     }
