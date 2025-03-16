949f53ee5cf8c5b86d5ceb9b8d0438bacd92c4d4
MaxMad446 <puper-1981@mail.ru>
2025-03-16 17:24:35 +0300

    Fix edge case logic

--- src/Generated20260511.cpp
+++ src/Generated20260511.cpp
@@ -4,14 +4,14 @@ using namespace std;
 
 class Generated20260511 {
 public:
-    // Generated class 20260511_173402_645686
-    int value = 316;
-    string created = "20260511_173402_645686";
+    // Generated class 20260511_173421_192375
+    int value = 211;
+    string created = "20260511_173421_192375";
     
     int process() {
         int result = value;
-        for (int i = 0; i < 32; i++) {
-            result += i * 6;
+        for (int i = 0; i < 5; i++) {
+            result += i * 9;
         }
         return result;
     }
