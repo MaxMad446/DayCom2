d82b8b8e092503d4af4bdfb772960ea1a0e69024
MaxMad446 <puper-1981@mail.ru>
2025-03-17 11:54:45 +0300

    Optimize loops

--- src/generated_function_20260511.js
+++ src/generated_function_20260511.js
@@ -1,11 +1,11 @@
 class Generated20260511 {
-    // Generated class 20260511_173418_649348
+    // Generated class 20260511_173425_848512
     constructor() {
-        this.value = 123;
-        this.created = "20260511_173418_649348";
+        this.value = 655;
+        this.created = "20260511_173425_848512";
     }
 
     process() {
-        return this.value * 8;
+        return this.value * 4;
     }
 }
