30db6297e7c383254e7721c1a92b591a7e3f0130
MaxMad446 <puper-1981@mail.ru>
2025-03-18 11:23:28 +0300

    Optimize loops

--- src/generated_function_20260511.py
+++ src/generated_function_20260511.py
@@ -1,6 +1,6 @@
 def generated_function_20260511():
-    # Generated function 20260511_173433_245608
-    result = 830
-    for i in range(9):
-        result += i * 6
+    # Generated function 20260511_173435_904889
+    result = 607
+    for i in range(14):
+        result += i * 10
     return result
