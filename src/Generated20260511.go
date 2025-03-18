123282b57a8c78f3708f7c52e657a43c8a75cf72
MaxMad446 <puper-1981@mail.ru>
2025-03-18 12:35:34 +0300

    Fix edge case logic

--- src/Generated20260511.go
+++ src/Generated20260511.go
@@ -3,20 +3,20 @@ package main
 import "fmt"
 
 type Generated20260511 struct {
-    // Generated struct 20260511_173432_115750
+    // Generated struct 20260511_173434_644795
     Value int
     Created string
 }
 
 func (c *Generated20260511) Process() int {
     result := c.Value
-    for i := 0; i < 47; i++ {
-        result += i * 7
+    for i := 0; i < 15; i++ {
+        result += i * 9
     }
     return result
 }
 
 func main() {
-    obj := Generated20260511{Value: 735, Created: "20260511_173432_115750"}
+    obj := Generated20260511{Value: 656, Created: "20260511_173434_644795"}
     fmt.Println(obj.Process())
 }
