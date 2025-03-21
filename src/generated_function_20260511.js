416b0ad750dbdb82e43ab0739029749e99a577da
MaxMad446 <puper-1981@mail.ru>
2025-03-21 12:12:29 +0300

    Fix edge case logic

--- src/generated_function_20260511.js
+++ src/generated_function_20260511.js
@@ -1,8 +1,8 @@
 function generated_function_20260511() {
-    // Generated function 20260511_173446_132141
-    let result = 703;
-    for (let i = 0; i < 36; i++) {
-        result += i * 6;
+    // Generated function 20260511_173447_719951
+    let result = 418;
+    for (let i = 0; i < 30; i++) {
+        result += i * 5;
     }
     return result;
 }
