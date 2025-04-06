e046796346a645ae16d7e22eab01342b5d0b15e9
MaxMad446 <puper-1981@mail.ru>
2025-04-06 16:21:21 +0300

    Add documentation

--- src/generated_function_20260511.js
+++ src/generated_function_20260511.js
@@ -1,8 +1,11 @@
-function generated_function_20260511() {
-    // Generated function 20260511_173601_051271
-    let result = 336;
-    for (let i = 0; i < 12; i++) {
-        result += i * 2;
+class Generated20260511 {
+    // Generated class 20260511_173602_791438
+    constructor() {
+        this.value = 918;
+        this.created = "20260511_173602_791438";
     }
-    return result;
+
+    process() {
+        return this.value * 2;
+    }
 }
