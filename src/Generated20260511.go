1bd4e242577957ad055fb26d274db472c309f23b
MaxMad446 <puper-1981@mail.ru>
2025-05-09 18:56:20 +0300

    Add documentation

--- src/Generated20260511.go
+++ src/Generated20260511.go
@@ -3,20 +3,20 @@ package main
 import "fmt"
 
 type Generated20260511 struct {
-    // Generated struct 20260511_173827_181607
+    // Generated struct 20260511_173841_079836
     Value int
     Created string
 }
 
 func (c *Generated20260511) Process() int {
     result := c.Value
-    for i := 0; i < 15; i++ {
-        result += i * 4
+    for i := 0; i < 40; i++ {
+        result += i * 8
     }
     return result
 }
 
 func main() {
-    obj := Generated20260511{Value: 757, Created: "20260511_173827_181607"}
+    obj := Generated20260511{Value: 242, Created: "20260511_173841_079836"}
     fmt.Println(obj.Process())
 }
