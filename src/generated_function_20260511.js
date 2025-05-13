220ac414e5fac23d01fa4a752fb62c7488bbb775
MaxMad446 <puper-1981@mail.ru>
2025-05-13 09:38:38 +0300

    Add documentation

--- src/generated_function_20260511.js
+++ src/generated_function_20260511.js
@@ -1,8 +1,8 @@
 function generated_function_20260511() {
-    // Generated function 20260511_173851_581635
-    let result = 830;
-    for (let i = 0; i < 40; i++) {
-        result += i * 2;
+    // Generated function 20260511_173857_116322
+    let result = 263;
+    for (let i = 0; i < 34; i++) {
+        result += i * 5;
     }
     return result;
 }
