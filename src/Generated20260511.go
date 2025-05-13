a8e51a077d3b5a45cb98faee843984416b8080f1
MaxMad446 <puper-1981@mail.ru>
2025-05-13 12:07:20 +0300

    Refactor utility functions

--- src/Generated20260511.go
+++ src/Generated20260511.go
@@ -3,20 +3,20 @@ package main
 import "fmt"
 
 type Generated20260511 struct {
-    // Generated struct 20260511_173843_832623
+    // Generated struct 20260511_173856_362576
     Value int
     Created string
 }
 
 func (c *Generated20260511) Process() int {
     result := c.Value
-    for i := 0; i < 33; i++ {
-        result += i * 10
+    for i := 0; i < 15; i++ {
+        result += i * 2
     }
     return result
 }
 
 func main() {
-    obj := Generated20260511{Value: 540, Created: "20260511_173843_832623"}
+    obj := Generated20260511{Value: 490, Created: "20260511_173856_362576"}
     fmt.Println(obj.Process())
 }
