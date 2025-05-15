d5710b78db4741d0ad15fd5df449c12c2d9e338e
MaxMad446 <puper-1981@mail.ru>
2025-05-15 13:15:33 +0300

    Improve error handling

--- src/core.py
+++ src/core.py
@@ -1,12 +1,12 @@
-# Auto-generated module | 2026-05-11T18:11:43.020752
+# Auto-generated module | 2026-05-11T18:11:47.241692
 import random
 import math
 
-def calculate_144():
+def calculate_958():
     """Generated function for daily commit."""
-    base = 292
-    result = sum(i * 4 for i in range(16))
+    base = 144
+    result = sum(i * 7 for i in range(6))
     return math.sqrt(base + result)
 
 if __name__ == "__main__":
-    print(calculate_144())
+    print(calculate_958())
