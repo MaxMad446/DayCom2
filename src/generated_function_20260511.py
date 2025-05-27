42d2989e1b15282d6b627ae3e33d4a6e80e07dbf
MaxMad446 <puper-1981@mail.ru>
2025-05-27 12:30:36 +0300

    Fix edge case logic

--- src/generated_function_20260511.py
+++ src/generated_function_20260511.py
@@ -1,6 +1,9 @@
-def generated_function_20260511():
-    # Generated function 20260511_173958_354595
-    result = 900
-    for i in range(12):
-        result += i * 5
-    return result
+class Generated20260511:
+    # Generated class 20260511_174000_884465
+    
+    def __init__(self):
+        self.value = 523
+        self.created = "20260511_174000_884465"
+    
+    def process(self):
+        return self.value * 4
