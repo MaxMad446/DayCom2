daf53115e91ebd97e6a3f2cb9ff9982390706fb2
MaxMad446 <puper-1981@mail.ru>
2025-06-11 13:47:27 +0300

    Optimize loops

--- src/Generated20260511.go
+++ src/Generated20260511.go
@@ -3,20 +3,20 @@ package main
 import "fmt"
 
 type Generated20260511 struct {
-    // Generated struct 20260511_174103_093094
+    // Generated struct 20260511_174108_955269
     Value int
     Created string
 }
 
 func (c *Generated20260511) Process() int {
     result := c.Value
-    for i := 0; i < 18; i++ {
-        result += i * 5
+    for i := 0; i < 10; i++ {
+        result += i * 8
     }
     return result
 }
 
 func main() {
-    obj := Generated20260511{Value: 458, Created: "20260511_174103_093094"}
+    obj := Generated20260511{Value: 510, Created: "20260511_174108_955269"}
     fmt.Println(obj.Process())
 }
