c325008df6311655c64e059b2097b660875df430
MaxMad446 <puper-1981@mail.ru>
2025-11-21 13:02:20 +0300

    Clean up unused code

--- src/core.py
+++ src/core.py
@@ -1,12 +1,12 @@
-# Auto-generated module | 2026-05-11T18:36:37.772072
+# Auto-generated module | 2026-05-11T18:36:39.909763
 import random
 import math
 
-def calculate_916():
+def calculate_635():
     """Generated function for daily commit."""
-    base = 271
-    result = sum(i * 2 for i in range(6))
+    base = 187
+    result = sum(i * 3 for i in range(22))
     return math.sqrt(base + result)
 
 if __name__ == "__main__":
-    print(calculate_916())
+    print(calculate_635())
