946f3b5f10d7889295e73ceb185c69a3ae399afb
MaxMad446 <puper-1981@mail.ru>
2026-01-26 06:20:33 +0300

    Fix bugs and improve performance

--- src/Generated20260126.java
+++ src/Generated20260126.java
@@ -1,13 +1,13 @@
 public class Generated20260126 {
-    // Generated class 20260126_061837_929400
+    // Generated class 20260126_062033_295211
     
-    private int value = 783;
-    private String created = "20260126_061837_929400";
+    private int value = 320;
+    private String created = "20260126_062033_295211";
     
     public int process() {
         int result = value;
-        for (int i = 0; i < 19; i++) {
-            result += i * 7;
+        for (int i = 0; i < 12; i++) {
+            result += i * 5;
         }
         return result;
     }
