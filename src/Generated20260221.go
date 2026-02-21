6f4ecb0694de9adb3456067825f027ca6e5e36f0
MaxMad446 <puper-1981@mail.ru>
2026-02-21 11:13:51 +0300

    Fix bugs and improve performance

--- src/Generated20260221.go
+++ src/Generated20260221.go
@@ -3,20 +3,20 @@ package main
 import "fmt"
 
 type Generated20260221 struct {
-    // Generated struct 20260221_120441_924891
+    // Generated struct 20260221_142941_088821
     Value int
     Created string
 }
 
 func (c *Generated20260221) Process() int {
     result := c.Value
-    for i := 0; i < 11; i++ {
+    for i := 0; i < 10; i++ {
         result += i * 4
     }
     return result
 }
 
 func main() {
-    obj := Generated20260221{Value: 466, Created: "20260221_120441_924891"}
+    obj := Generated20260221{Value: 292, Created: "20260221_142941_088821"}
     fmt.Println(obj.Process())
 }
