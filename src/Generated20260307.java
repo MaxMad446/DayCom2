c2fbe3553d747304a805470ad892840815e569f2
MaxMad446 <puper-1981@mail.ru>
2026-03-07 09:06:09 +0300

    Optimize algorithms and data structures

--- src/Generated20260307.java
+++ src/Generated20260307.java
@@ -1,13 +1,13 @@
 public class Generated20260307 {
-    // Generated class 20260307_090517_488040
+    // Generated class 20260307_090609_685404
     
-    private int value = 977;
-    private String created = "20260307_090517_488040";
+    private int value = 503;
+    private String created = "20260307_090609_685404";
     
     public int process() {
         int result = value;
-        for (int i = 0; i < 38; i++) {
-            result += i * 10;
+        for (int i = 0; i < 23; i++) {
+            result += i * 4;
         }
         return result;
     }
