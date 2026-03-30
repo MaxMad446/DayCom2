0a95ef02d241e324b9d3e5a981793b108c043e35
MaxMad446 <puper-1981@mail.ru>
2026-03-30 17:04:34 +0300

    Refactor code for better readability

---
+++ src/Generated20260330.java
@@ -0,0 +1,14 @@
+public class Generated20260330 {
+    // Generated class 20260330_190328_341792
+    
+    private int value = 982;
+    private String created = "20260330_190328_341792";
+    
+    public int process() {
+        int result = value;
+        for (int i = 0; i < 21; i++) {
+            result += i * 3;
+        }
+        return result;
+    }
+}
